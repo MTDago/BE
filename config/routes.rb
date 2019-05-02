db5ee8f4b87d480875705858906dd94243d1871c
Matthew Leacy <matthew.leacy@live.com.au>
2019-05-02 15:39:54 +1000

    further devise work

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :users
+
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
+
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
+  
   resources :listings
   resources :users
   resources :models
