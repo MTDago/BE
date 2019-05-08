f4ec562f9c5a2cecb323d3957b33483b65f76e2a
Matt-Tee <47690918+Matt-Tee@users.noreply.github.com>
2019-05-08 12:49:28 +1000

    title search feature integrated (#17)

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,3 +1,12 @@
 class Listing < ApplicationRecord
     belongs_to :user
+
+    def self.search(term)
+        if term
+          where('title LIKE ?', "%#{term}%").order('id DESC')
+        else
+          order('id DESC') 
+        end
+    end
+
 end
